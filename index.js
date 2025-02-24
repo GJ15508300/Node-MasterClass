4dc4e9bf5e6c098c3b89fe149214403573ae656e
Jagadeesh GJ <jagadeesh15508300@gmail.com>
2025-02-24 19:31:29 +0530

    File System read and handle error in file DONE

--- index.js
+++ index.js
@@ -9,17 +9,34 @@
 //   })
 //   .listen(8080);
 
-//Split Query
-var http = require("http");
-var url = require("url");
+// //Split Query
+// var http = require("http");
+// var url = require("url");
 
+// http
+//   .createServer(function (req, res) {
+//     res.writeHead(200, { "Content-Type": "text/html" });
+//     var q = url.parse(req.url, true).query;
+//     var txt = q.year + " " + q.month;
+//     res.end(txt);
+//   })
+//   .listen(8080);
+
+// // http://localhost:8080/?year=2017&month=July
+
+const http = require("http");
+const fs = require("fs");
 http
   .createServer(function (req, res) {
-    res.writeHead(200, { "Content-Type": "text/html" });
-    var q = url.parse(req.url, true).query;
-    var txt = q.year + " " + q.month;
-    res.end(txt);
+    fs.readFile("demoFile1.html", function (err, data) {
+      if (err) {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.write("Error File");
+        return res.end();
+      }
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write(data);
+      return res.end();
+    });
   })
   .listen(8080);
-
-// http://localhost:8080/?year=2017&month=July
