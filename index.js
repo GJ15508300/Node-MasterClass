66a4b75bce0c1b93f7f7bdeaefa2366b5d82dfaa
Jagadeesh GJ <jagadeesh15508300@gmail.com>
2025-02-24 17:42:56 +0530

    first module created and tested successfully

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 var http = require("http");
+var dt = require("./myFirstModule");
 
-http.createServer(function (req, res) {
-  res.writeHead(200, { "Content-Type": "text/html" });
-  res.end("Hello World");
-}).listen(8080);
+http
+  .createServer(function (req, res) {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write(`The date and time are currently : ${dt?.myDateTime()}`);
+    res.end("Hello World");
+    // res.end();
+  })
+  .listen(8080);
