d851a0597034f4a711efa5a207c6b17ed5d2ba55
Jagadeesh GJ <jagadeesh15508300@gmail.com>
2025-02-24 18:27:45 +0530

    Split Query

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
+// var http = require("http");
+// var dt = require("./myFirstModule");
+
+// http
+//   .createServer(function (req, res) {
+//     res.writeHead(200, { "Content-Type": "text/html" });
+//     res.write(`The date and time are currently : ${dt?.myDateTime()}`);
+//     res.end("Hello World");
+//   })
+//   .listen(8080);
+
+//Split Query
 var http = require("http");
-var dt = require("./myFirstModule");
+var url = require("url");
 
 http
   .createServer(function (req, res) {
     res.writeHead(200, { "Content-Type": "text/html" });
-    res.write(`The date and time are currently : ${dt?.myDateTime()}`);
-    res.end("Hello World");
-    // res.end();
+    var q = url.parse(req.url, true).query;
+    var txt = q.year + " " + q.month;
+    res.end(txt);
   })
   .listen(8080);
+
+// http://localhost:8080/?year=2017&month=July
